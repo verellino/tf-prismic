e2c2568ee2299ec4f587121c37bc925fa92a5bc9
Mekel Verellino <verellino@gmail.com>
2022-10-13 19:10:36 +0700

    mentor links

--- components/mentor/Grid.vue
+++ components/mentor/Grid.vue
@@ -8,11 +8,11 @@
           :key="mentor.id"
           :mentor="mentor"
         />
-        <button
+        <a href="https://forms.gle/z9byuyAr3eSBjhyJ8"
           class="w-full mx-auto mt-8 rounded-md bg-maroon px-4 py-2 text-xs text-white"
         >
           <span>Ingin jadi mentee? Daftar di sini</span>
-        </button>
+        </a>
       </ul>
       <ul class="m-4 w-1/2 border p-4">
         <h2 class="section-title text-center">Mentor Private Sector</h2>
@@ -21,11 +21,11 @@
           :key="mentor.id"
           :mentor="mentor"
         />
-        <button
+        <a href="https://forms.gle/xmu35RckGbTjXMyB6"
           class="w-full mx-auto mt-8 rounded-md bg-maroon px-4 py-2 text-xs text-white"
         >
           <span>Ingin jadi mentee? Daftar di sini</span>
-        </button>
+        </a>
       </ul>
     </div>
   </div>
